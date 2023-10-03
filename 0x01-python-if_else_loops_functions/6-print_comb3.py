5b679b913ebb5c800985e45e7d31c357055ecfb3
colly-m <sindanicolly@gmail.com>
2023-10-03 04:51:49 -0700

    Brains not matter

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+output = ""
 for fir_digit in range(10):
     for sec_digit in range(fir_digit + 1, 10):
-        print("{}{}, ".format(fir_digit, sec_digit), end="")
+        output += "{:02d}, ".format(fir_digit * 10 + sec_digit)
 
-print()
+print(output[:-2])
