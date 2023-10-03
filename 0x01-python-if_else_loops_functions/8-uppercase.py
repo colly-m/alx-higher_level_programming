6cbc440e2ca68a955b41aea3c59ea0233d6a9d9e
colly-m <sindanicolly@gmail.com>
2023-10-03 07:35:41 -0700

    Change to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for char in s:
-        if 'a' <= char <= 'z':
-            uppercase_char = chr(ord(char) - 32)
+def uppercase(str):
+    change_case = 0
+    for letter in str:
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+            change_case = 32
         else:
-            uppercase_char = char
-        print(uppercase_char, end='')
+            change_case = 0
+        print('{:c}'.format(ord(letter) - change_case), end="")
     print()
