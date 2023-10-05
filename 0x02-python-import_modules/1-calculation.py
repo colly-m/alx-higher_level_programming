9667501529e26cd260113c7dc7e6b71b2b7b4956
colly-m <sindanicolly@gmail.com>
2023-10-05 02:45:58 -0700

    My toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,12 +4,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    res_add = add(a, b)
-    res_sub = sub(a, b)
-    res_mul = mul(a,b)
-    res_div = div(a, b)
-
-    print("{} + {} = {}".format(a, b, res_add))
-    print("{} - {} = {}".format(a, b, res_sub))
-    print("{} * {} = {}".format(a, b, res_mul))
-    print("{} / {} = {}".format(a, b, res_div))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
