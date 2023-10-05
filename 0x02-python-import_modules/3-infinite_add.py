c008ed5392bdff7deccc3e0d2bf9c1804125c1bb
colly-m <sindanicolly@gmail.com>
2023-10-05 03:51:25 -0700

    Add till infinity

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    args = sys.argv[1:]
+
+    sum = 0
+
+    for arg in args:
+        sum += int(arg)
+
+    print(sum)
+
+if __name__ == "__main__":
+    main()
