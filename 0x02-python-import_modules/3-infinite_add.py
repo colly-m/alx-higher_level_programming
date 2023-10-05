c49a7d190599f16a2efa69d4a5ec7445cdb07ff5
colly-m <sindanicolly@gmail.com>
2023-10-05 04:37:51 -0700

    Add to infinity

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-def main():
-    args = sys.argv[1:]
+if __name__ == "__main__":
 
     result = 0
-
-    for arg in args:
-        result += int(arg)
-
-    print("{:d}".format(result))
-
-if __name__ == "__main__":
-    main()
+    for a in range(len(sys.argv) - 1):
+        result += int(sys.argv[a + 1])
+    print("{}".format(result))
