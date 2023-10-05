44800c842f5f1fd936b6a3cf2daa88a5b0469b2c
colly-m <sindanicolly@gmail.com>
2023-10-05 02:34:31 -0700

    My tools

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+
+    res_add = add(a, b)
+    res_sub = sub(a, b)
+    res_mul = mul(a,b)
+    res_div = div(a, b)
+
+    print(f"{a} + {b}: {res_add}")
+    print(f"{a} - {b}: {res_sub}")
+    print(f"{a} * {b}: {res_mul}")
+    print(f"{a} / {b}: {res_div}")
