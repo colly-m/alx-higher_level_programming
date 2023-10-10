1cf6206ff524ae321686548f0119fc8e786f1eec
colly-m <sindanicolly@gmail.com>
2023-10-10 12:59:08 -0700

    The snake list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,4 @@
 #include <Python.h>
-#include <object.h>
-#include <listobject.h>
 
 /**
  * print_python_list_info - function to print info on python list
@@ -10,17 +8,17 @@
 void print_python_list_info(PyObject *p)
 {
 	PyObject *ob;
-	int s, t, u;
+	Py_ssize_t s, t, u;
 
 	s = Py_SIZE(p);
-	t - ((PyListObject *)p)->allocated;
+	t - ((PyListObject *)p)->t;
 
-	printf("[*] Size of the Python List = %d\n", s);
-	printf("[*] Allocated = %d\n", t);
+	printf("[*] Size of the Python List = %ld\n", s);
+	printf("[*] Allocated = %ld\n", t);
 
 	for (u = 0; u < s; u++)
 	{
-		printf("Element %d: ", u);
+		printf("Element %ld: ", u);
 
 		ob = PyList_GetItem(p, u);
 		printf("%s\n", Py_TYPE(ob)->tp_name);
