bb599dec48ecdee2795237b606cf8a1a4b8f49d4
colly-m <sindanicolly@gmail.com>
2023-10-11 02:56:20 -0700

    Matrix squared

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    res_matrix = [row[:] for row in matrix]
-    for idx1, row in enumerate(res_matrix):
-        for idx2, clmn in enumerate(res_matrix):
-            res_matrix[idx1][idx2] = row[idx2] ** 2
+    res_matrix = [[x ** 2 for x in row] for row in matrix]
 
     return (res_matrix)
