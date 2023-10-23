9b1cc0be782291954581b4c21a66914a366593bb
colly-m <sindanicolly@gmail.com>
2023-10-23 12:34:58 -0700

    Divide and debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return (None)
+    finally:
+        if 'result' in locals():
+            print("Inside result: {}".format(result))
+        else:
+            print("Inside result: None")
