388a7b4cf01a103d99c55e634d4c99cacf55a82c
colly-m <sindanicolly@gmail.com>
2023-10-23 16:45:00 -0700

    Safety func

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        outcome = fct(*args)
+        return (outcome)
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return (None)
