fdc49c3df3b9860dbfccbd0820b7c424af46a94d
colly-m <sindanicolly@gmail.com>
2023-10-23 12:41:28 -0700

    Divide N debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,9 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        return (None)
+        result = None
     finally:
-        if 'result' in locals():
-            print("Inside result: {}".format(result))
-        else:
-            print("Inside result: None")
+        print("Inside result: {}".format(result))
+    return (result)
