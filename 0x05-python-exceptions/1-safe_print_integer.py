4ef22c441bba501e5772dc864ff20b2c536e241e
colly-m <sindanicolly@gmail.com>
2023-10-23 10:24:09 -0700

    Safe int print

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        val = "{:d}".format(value)
+        print(val)
+
+        return (True)
+    except (ValueError, TypeError):
+        return (False)
