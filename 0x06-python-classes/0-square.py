6c04268b8d87918493f0a0650205864827e61ac7
colly-m <sindanicolly@gmail.com>
2023-10-24 05:56:08 -0700

    First square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,7 +2,5 @@
 """Square class defined"""
 
 class Square:
-    """
-    Square class body, representing a square shape.
-    """
+    """Square class body, representing a square shape."""
     pass
