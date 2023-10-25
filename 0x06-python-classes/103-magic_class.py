268377cc51c9616d8ad977b878bd98934307c9a0
colly-m <sindanicolly@gmail.com>
2023-10-25 02:37:03 -0700

    Get around it

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,10 +7,8 @@ class MagicClass:
     """A class to compute area and circumference of a acircle"""
     def __init__(self, radius=0):
         """
-        Initializes the MagicClass with a given radius
-        Arguments of the raduis should be float
-        TypeError if radius is not number
-        """
+        Initializes the MagicClass with a given radius"""
+        self.__radius = 0
         if not (isisnstance(radius, int) or isinstance(radius, float)):
             raise TypeError("radius must be a number")
         self.__radius = radius
