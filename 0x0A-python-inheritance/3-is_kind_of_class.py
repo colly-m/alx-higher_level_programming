3e38e80d25b2681c0040551cc8742a50b5cf5496
colly-m <sindanicolly@gmail.com>
2023-11-06 13:16:03 -0800

    Simmilarity update

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,8 +5,5 @@
 def is_kind_of_class(obj, a_class):
     """Defines the class an object belongs to or if object is a class"""
     if isinstance(obj, a_class):
-        return True
-    for cls in obj.__class__.__bases__:
-        if is_kind_of_class(obj, cls):
-            return True
-    return False
+        return (True)
+    return (False)
