b623a872a7b7fa0a65e4ca8cdaac8d3dc2dd1130
colly-m <sindanicolly@gmail.com>
2023-11-06 13:45:56 -0800

    Geometry imroved update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to write a classBaseGeometry based on another"""
+
+
+class BaseGeometry:
+    """A class BaseGeometry body"""
+    def area(self):
+        raise Exception("area() is not implemented")
