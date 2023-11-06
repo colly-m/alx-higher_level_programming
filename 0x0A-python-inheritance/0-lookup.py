92d636afee20a3884278b6d0c0ef5ee96a0d48f4
colly-m <sindanicolly@gmail.com>
2023-11-06 04:46:01 -0800

    Lookup update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,7 +5,4 @@
 def lookup(obj):
     """Returns a list object of available attributes and methods of an object"""
     attrib = dir(obj)
-    """Separate attributes and methods starting with '__'"""
-    attribute = [item for item in attrib if not item.startswith('_')]
-
-    return (attribute)
+    return (attrib)
