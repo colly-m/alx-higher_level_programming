948d429acc4b2a039e153ef74a612346d62c0f8a
colly-m <sindanicolly@gmail.com>
2023-11-07 13:19:21 -0800

    Object saved to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """using with to access the file"""
     with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file,ensure_ascii=False)
+        json.dump(my_obj, file, ensure_ascii=False)
