49e92e56928aabcbf5a9d02304f1a38876a9378f
colly-m <sindanicolly@gmail.com>
2023-11-07 12:42:35 -0800

    JSON stringed

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function to return JSON string representation"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Module returns a JSOobject string"""
+    return (json.dumps(my_obj))
