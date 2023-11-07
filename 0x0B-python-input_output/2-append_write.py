ea1547baa330fe53bef4cf3967de3f43f441cb86
colly-m <sindanicolly@gmail.com>
2023-11-07 12:15:07 -0800

    Append to file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,11 +4,6 @@
 
 
 def append_write(filename="", text=""):
-    """Count the number of characters to be added"""
-    char_count = len(text)
-
     """Use the with statement to automatically close the file after writing"""
-    with open(filename, 'a', encoding='utf8') as file:
-        file.write(text)
-
-    return (char_count)
+    with open(filename, 'a', encoding='utf-8') as file:
+        return (my_file.write(text))
