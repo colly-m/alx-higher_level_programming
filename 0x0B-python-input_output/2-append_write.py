c313ddde0d815e173eae662563e00cfa87efdb75
colly-m <sindanicolly@gmail.com>
2023-11-07 11:52:48 -0800

    Append to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function to append a string at end of a text file"""
+
+
+
+def append_write(filename="", text=""):
+    """Count the number of characters to be added"""
+    char_count = len(text)
+
+    """Use the with statement to automatically close the file after writing"""
+    with open(filename, 'a', encoding='utf8') as file:
+        file.write(text)
+
+    return (char_count)
