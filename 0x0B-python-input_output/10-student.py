448eea3bc622a1bdc6b13862526acda271575813
colly-m <sindanicolly@gmail.com>
2023-11-08 08:42:27 -0800

    With filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module defines a class Student based on another"""
+
+
+class Student:
+    """Represents a class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student props"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retries a dictionary representation of a student instance"""
+        if (type(attrs) == list and
+                all(type(elem) == str for elem in attrs)):
+            return {n: getattr(self, n) for n in attrs if hasattr(self, n)}
+        return (self.__dict__)
