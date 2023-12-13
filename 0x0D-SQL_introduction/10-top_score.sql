f55f8207690c824563ec79576472685b4a1d0e11
colly-m <sindanicolly@gmail.com>
2023-12-13 15:20:25 -0800

    Best listing

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script to list all records of a table of the database in MySQL server
+-- Should display both score and name respectively and records to be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
