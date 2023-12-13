09eebddbf11717861c2c32e1c81df0a41e286caf
colly-m <sindanicolly@gmail.com>
2023-12-13 14:58:15 -0800

    Addition one

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Script to insert new row in the first table database in MySQL server
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
