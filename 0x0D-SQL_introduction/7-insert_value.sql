c09da0a56b7e783b5e8b6d5591b8d0db392a3833
colly-m <sindanicolly@gmail.com>
2023-12-13 14:56:12 -0800

    The 89th

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Script to insert new row in the first table database in MySQL server
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "BEST SCHOOL");
+INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Script to display number of records with id = 89 in a table from database in MySQL server
+SELECT COUNT(*) FROM first_table WHERE id = 89;
