2cd8b3295209fb48200b0fad7d2194bbe99eb98b
colly-m <sindanicolly@gmail.com>
2023-12-14 12:51:32 -0800

    West coasting

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,5 @@
 -- Script to list all cities of California found in database 'hbtn_0d_usa'
-USE hbtn_0d_usa;
 
--- Select all cities in California sorted by cities.id
-SELECT * FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+SELECT id,
+name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
