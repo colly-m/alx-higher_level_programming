4cf7679695570a3fc595f6f4cae584066701387f
colly-m <sindanicolly@gmail.com>
2023-12-14 10:41:35 -0800

    Read up user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Script to create a database 'hbtn_0d_2' and user 'user_0d_2'
+
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
