e4404021a9ae9c1eef2cb132ee478165d1d1674a
colly-m <sindanicolly@gmail.com>
2023-12-14 14:59:57 -0800

    Tomato tomato

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Script to list all shows by rating in 'hbtn_0d_tvshows_rate' database in MySQL server
+
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
