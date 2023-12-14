4802cad35514dd8687b4f9024950f03f8fc5bb44
colly-m <sindanicolly@gmail.com>
2023-12-14 11:14:39 -0800

    Siempre un nombre

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,2 @@
 -- Script to create a table 'force_name' on MySQL server
-CREATE TABLE IF NOT EXIST force_name (
-	id INT,
-	name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXIST force_name (id INT, name VARCHAR(256) NOT NULL);
