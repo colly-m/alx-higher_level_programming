433687cbca2317e5cfc80704d681d00b8cc8ba55
colly-m <sindanicolly@gmail.com>
2023-12-14 10:45:44 -0800

    Read up user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
--- Script to create a database 'hbtn_0d_2' and user 'user_0d_2'
+-- Script to create a database 'hbtn_0d_2' and user 'user_0d_2' with read priviledge
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Creates user
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grants priviledge
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
