b5f759db2cc0e21b08393c2009fd8eeecc62685a
colly-m <sindanicolly@gmail.com>
2023-12-14 10:47:29 -0800

    Read up user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creates user
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Grants priviledge
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
