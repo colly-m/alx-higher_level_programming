f3dc3af561ed57558207bee8ee1c4e05a8f4cae6
colly-m <sindanicolly@gmail.com>
2023-12-14 13:06:06 -0800

    City bye states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- Script to list all cities in a database 'hbtn_0d_usa'
 
-SELECT cities.id, cities.name, states.name,
-FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 LEFT JOIN states ON state.id = cities.state_id
 ORDER BY cities.id ASC;
