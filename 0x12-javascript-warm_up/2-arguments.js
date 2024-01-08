a9406cb35adc376a364940057ea242c947d9e4bc
colly-m <sindanicolly@gmail.com>
2024-01-08 12:33:25 -0800

    Argument back to back

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
-const numArgs = process.argv.length - 2;
-if (numArgs === 0)
-{
+const numArgs = process.argv.length;
+if (numArgs === 2) {
 	console.log('No argument');
 }
-else if (numArgs === 1)
-{
+else if (numArgs === 3) {
 	console.log('Argument found');
 }
-else
-{
+else {
 	console.log('Argument found');
 }
