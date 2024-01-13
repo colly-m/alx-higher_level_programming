15d441246bc2002abd710afd30ab398c9236dcd1
colly-m <sindanicolly@gmail.com>
2024-01-13 13:31:45 -0800

    State N filtered

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script lists all states names starting with 'N' from 'hbtn_0e_0_usa' database"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """Accesses database server"""
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ;"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(rows)
