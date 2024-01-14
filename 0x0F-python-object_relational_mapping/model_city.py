c98a8b36224693b603fe0619c18b88050675d6f0
colly-m <sindanicolly@gmail.com>
2024-01-14 08:55:13 -0800

    Cities in state update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,11 +6,15 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 class City(Base):
-    """City class to interact with database"""
+    """
+    City class to interact with database id (int): id of classs
+    state_id (int): state city belongs to
+    name (str): name of class
+    """
 
     __tablename__ = 'cities'
 
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
