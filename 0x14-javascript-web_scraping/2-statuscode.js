ca2e1a1f09f8386f349d90ac2a9fa4ef340337e4
colly-m <sindanicolly@gmail.com>
2024-02-20 15:41:44 -0800

    Star wars binge title

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
+request.get(url, (err, response) => {
+  if (err) {
+    console.log(err);
   } else {
     console.log(`code: ${response.statusCode}`);
   }
