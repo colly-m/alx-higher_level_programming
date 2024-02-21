bdd90e8d828ccc72ec62e4ce400287aa5f4c3d4f
colly-m <sindanicolly@gmail.com>
2024-02-21 13:41:10 -0800

    Correct order update fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,31 +1,34 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let movid = parseInt(process.argv[2], 10);
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movId}/`;
 let characters = [];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const res = JSON.parse(body);
-    const results = res.results;
-    if (id < 4) {
-      movid += 3;
-    } else {
-      movid -= 3;
-    }
-    for (let m = 0; m < results.length; m++) {
-      if (results[m].episode_id === movid) {
-        characters = results[m].characters;
-        break;
-      }
-    }
-    for (let c = 0; c < characters.length; c++) {
-      request(characters[c], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
   }
+
+  const data = JSON.parse(body);
+  characters = data.characters;
+  getCharacters(0);
 });
+
+const getCharacters = (idx) => {
+  if (idx === characters.length) {
+    return;
+  }
+
+  request(characters[idx], (err, response, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    getCharacters(idx + 1);
+  });
+};
